6ea0d012acdd49736b98b274d84d8a84ebc367b3
Hamid <www.abdulhamid3311@gmail.com>
2022-05-12 23:28:04 +0600

    added google auth

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
+    apiKey: process.env.REACT_APP_api_Key,
     authDomain: process.env.REACT_APP_authDomain,
     projectId: process.env.REACT_APP_projectId,
     storageBucket: process.env.REACT_APP_storageBucket,
--- src/pages/Page/Login/Login.js
+++ src/pages/Page/Login/Login.js
@@ -1,39 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (user) {
+        console.log(user);
+    }
     return (
-        <div class="hero min-h-screen">
-            <div class="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+        <div className="hero min-h-screen">
+            <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
                 <h3 className='text-secondary text-3xl mx-auto mt-4'>Please Login</h3>
-                <div class="card-body">
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Email</span>
+                <div className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
                         </label>
-                        <input type="text" placeholder="email" class="input input-bordered" />
+                        <input type="text" placeholder="email" className="input input-bordered" />
                     </div>
 
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Password</span>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
                         </label>
-                        <input type="text" placeholder="password" class="input input-bordered" />
-                        <label class="label">
-                            <Link to={'/'} class="label-text-alt link link-hover">Forgot password?</Link>
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                        <label className="label">
+                            <Link to={'/'} className="label-text-alt link link-hover">Forgot password?</Link>
                         </label>
                     </div>
 
-                    <div class="form-control mt-6">
-                        <button class="btn btn-accent">Login</button>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-accent">Login</button>
                     </div>
 
                     <p className='text-center'>New to Doctors portal?
                         <Link to={'/register'} className='text-secondary ml-2'>please register</Link>
                     </p>
 
-                    <div class="divider">OR</div>
-                    <button class="btn btn-outline btn-accent w-full">
+                    <div className="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className="btn btn-outline btn-accent w-full">
                         Continue with Google
                     </button>
                 </div>
