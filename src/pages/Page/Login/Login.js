4949ebe31b6e5b6703094b642961bde9bc09d633
Hamid <www.abdulhamid3311@gmail.com>
2022-05-13 12:04:29 +0600

    added toast,email varification

--- src/pages/Page/Login/Login.js
+++ src/pages/Page/Login/Login.js
@@ -1,41 +1,95 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import React, { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { async } from '@firebase/util';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Login = () => {
+    const [email, setEmail] = useState('');
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [
+        signInWithEmailAndPassword,
+        user1,
+        loading1,
+        error1,
+    ] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
+
     if (user) {
-        console.log(user);
+        navigate(from, { replace: true });
+    };
+    //erroe message
+    let errorElement;
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    };
+
+    const handleEmailBlur = e => {
+        setEmail(e.target.value);
+    }
+    //handle Login Form
+    const handleLoginForm = e => {
+        e.preventDefault();
+        const password = e.target.password.value;
+        console.log(email, password);
+        signInWithEmailAndPassword(email, password);
+        navigate('/appointment')
+    };
+    //Password Reset Email sending
+    const handleResetPass = async () => {
+        await sendPasswordResetEmail(email);
+        toast.warn('password sent')
     }
     return (
         <div className="hero min-h-screen">
             <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
                 <h3 className='text-secondary text-3xl mx-auto mt-4'>Please Login</h3>
                 <div className="card-body">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
-                    </div>
+                    <form onSubmit={handleLoginForm}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                onBlur={handleEmailBlur}
+                                required
+                                name='email'
+                                type="email"
+                                placeholder="email"
+                                className="input input-bordered" />
+                        </div>
 
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="text" placeholder="password" className="input input-bordered" />
-                        <label className="label">
-                            <Link to={'/'} className="label-text-alt link link-hover">Forgot password?</Link>
-                        </label>
-                    </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input required name='password' type="password" placeholder="password" className="input input-bordered" />
+                        </div>
+                        {errorElement}
 
-                    <div className="form-control mt-6">
-                        <button className="btn btn-accent">Login</button>
-                    </div>
+                        <div className="form-control mt-6">
+                            <input
+                                type="submit"
+                                value='Login'
+                                className="btn btn-accent"
+                            />
+                        </div>
+                    </form>
+
 
                     <p className='text-center'>New to Doctors portal?
                         <Link to={'/register'} className='text-secondary ml-2'>please register</Link>
+                    </p>
+                    <p className="text-center ">
+                        <button onClick={handleResetPass} className="label-text-alt link link-hover text-center">Forgot password?</button>
+                        <ToastContainer />
                     </p>
 
                     <div className="divider">OR</div>
--- src/pages/Page/Login/Register.js
+++ src/pages/Page/Login/Register.js
@@ -1,46 +1,68 @@
+import { async } from '@firebase/util';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigation } from 'react-day-picker';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const navigate = useNavigate()
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    const handleRegisterForm = async (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password);
+        await createUserWithEmailAndPassword(email, password);
+        navigate('/')
+
+
+    }
     return (
-        <div class="hero min-h-screen">
-            <div class="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+        <div className="hero min-h-screen">
+            <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
                 <h3 className='text-secondary text-3xl mx-auto mt-4'>Sign Up</h3>
-                <div class="card-body">
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Name</span>
-                        </label>
-                        <input type="text" placeholder="name" class="input input-bordered" />
-                    </div>
+                <div className="card-body">
+                    <form onSubmit={handleRegisterForm}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input name='name' type="text" placeholder="name" className="input input-bordered" />
+                        </div>
 
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Email</span>
-                        </label>
-                        <input type="email" placeholder="email" class="input input-bordered" />
-                    </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name='email' type="email" placeholder="email" className="input input-bordered" />
+                        </div>
 
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Password</span>
-                        </label>
-                        <input type="text" placeholder="password" class="input input-bordered" />
-                        <label class="label">
-                            <Link to={'/'} class="label-text-alt link link-hover">Forgot password?</Link>
-                        </label>
-                    </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name='password' type="password" placeholder="password" className="input input-bordered" />
+                        </div>
 
-                    <div class="form-control mt-6">
-                        <button class="btn btn-accent">Sign Up</button>
-                    </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-accent" type="submit" value="Sign Up" />
+                        </div>
 
+                    </form>
                     <p className='text-center'>Already have an account?
                         <Link to={'/login'} className='text-secondary ml-2'>please login</Link>
                     </p>
 
-                    <div class="divider">OR</div>
-                    <button class="btn btn-outline btn-accent w-full">
+                    <div className="divider">OR</div>
+                    <button className="btn btn-outline btn-accent w-full">
                         Continue with Google
                     </button>
                 </div>
