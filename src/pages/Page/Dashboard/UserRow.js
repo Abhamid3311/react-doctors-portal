b539c92deacb980c6478efe62b4acee8102f2917
Hamid <www.abdulhamid3311@gmail.com>
2022-05-23 11:46:41 +0600

    remove 75

---
+++ src/pages/Page/Dashboard/UserRow.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const UserRow = ({ user, refetch, index }) => {
+    const { email, role } = user;
+
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                if (data.modfiedCount > 0) {
+                    refetch();
+                    console.log(data);
+                    toast.success('Successfully made and admin');
+                };
+
+            })
+    };
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <td>{email}</td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
+            <td><button class="btn btn-xs">Delete User</button></td>
+
+
+        </tr>
+    );
+};
+
+export default UserRow;
