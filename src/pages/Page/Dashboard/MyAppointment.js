72e3a2f78c2a54e750bee4c2ab573cdd0c2420d4
Hamid <www.abdulhamid3311@gmail.com>
2022-06-02 20:33:07 +0600

    added jwt verify

--- src/pages/Page/Dashboard/MyAppointment.js
+++ src/pages/Page/Dashboard/MyAppointment.js
@@ -1,10 +1,14 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 const MyAppointment = () => {
     const [appointments, setAppointments] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+
     useEffect(() => {
         if (user) {
             fetch(`http://localhost:5000/booking?patient=${user.email}`, {
@@ -13,8 +17,19 @@ const MyAppointment = () => {
                     'authorization': `Bearer ${localStorage.getItem('accessToken')}`
                 }
             })
-                .then(res => res.json())
-                .then(data => setAppointments(data))
+                .then(res => {
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate("/")
+                    }
+                    return res.json();
+                })
+                .then(data => {
+
+                    setAppointments(data);
+                })
+
         }
     }, [user]);
     return (
