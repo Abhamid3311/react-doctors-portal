1c79b0f4797db8a7b8108002ad4f2e9a75da9666
Hamid <www.abdulhamid3311@gmail.com>
2022-09-24 07:28:56 +0600

    added add review and get review from DB

--- src/pages/HomePage/Testimonials/Testimonials.js
+++ src/pages/HomePage/Testimonials/Testimonials.js
@@ -18,7 +18,12 @@ import { Autoplay, Pagination, Navigation } from "swiper";
 const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
     useEffect(() => {
-        fetch("testimonial.json")
+        fetch("http://localhost:5000/review", {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
             .then(res => res.json())
             .then(data => setReviews(data))
     }, []);
--- src/pages/Page/Dashboard/MyReview.js
+++ src/pages/Page/Dashboard/MyReview.js
@@ -1,12 +1,72 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 const MyReview = () => {
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        const { name, city, review, img } = data;
+        const reviewDetails = {
+            name,
+            city,
+            review,
+            img
+        };
+
+
+        //Send To database
+        fetch('http://localhost:5000/review', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify(reviewDetails)
+        })
+            .then(res => res.json())
+            .then(inserted => {
+                if (inserted.insertedId) {
+                    toast.success('Review added SuccesFully');
+                    reset();
+                } else {
+                    toast.error('Failed To add Review!!');
+                }
+            })
+    };
+
+
     return (
-        <div>
-            <h2>Add Review</h2>
+        <div >
+            <h2 className='text-3xl font-semibold text-primary text-center my-5'>Add Review</h2>
+            <form onSubmit={handleSubmit(onSubmit)} className="w-1/2 mx-auto">
+                <input
+                    {...register("name")}
+                    placeholder="Name"
+                    class="input input-bordered input-info w-full max-w-md mb-5" required />
+
+                <input
+                    {...register("city")}
+                    placeholder="City"
+                    class="input input-bordered input-info w-full max-w-md mb-5" required />
+
+
+                <input
+                    {...register("review")}
+                    placeholder="Comments"
+                    class="input input-bordered input-info w-full max-w-md mb-5" required />
+
+                <input
+                    {...register("img")}
+                    placeholder="Img Url"
+                    class="input input-bordered input-info w-full max-w-md mb-5" required />
+
+                <input
+                    type="submit"
+                    value="submit"
+                    className='btn btn-primary w-full' />
+            </form>
 
         </div>
     );
-};
-
+}
 export default MyReview;
